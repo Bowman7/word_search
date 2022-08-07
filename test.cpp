ee039cf78945ef1a644be5e1f47acd66e80099bf
Bowman7 <ui14628@gmail.com>
2022-08-07 17:15:56 +0530

    a

--- test.cpp
+++ test.cpp
@@ -1,38 +1,6 @@
 #include<iostream>
 #include<random>
-int returnRand(int size)
-{
-	int val;
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution distrib(0,size);
-	val= distrib(gen);
-	return val;
-}
-int checkVal(int refVal,int p,int refAr[],int size)
-{
-	int checkVal=refVal;
-	for(int i=0;i<size;i++)
-	{
-		if(refAr[i]==checkVal)
-		{
-			checkVal=returnRand(size);
-		}
-	}
-	return checkVal;
-}
-void addRef(int baseAr[],int refAr[],int size)
-{
-	int refVal;
-	int retCheckVal;
-	for(int p=0;p<size;p++)
-	{
-		refVal=returnRand(size);
-		retCheckVal=checkVal(refVal,p,refAr,size);
-		refAr[p]=retCheckVal;
-
-	}
-}
+#include<vector>
 void printRef(int refAr[],int size)
 {
 	int count;
@@ -42,9 +10,43 @@ void printRef(int refAr[],int size)
 		count++;
 		if(count==3)
 		{
-			std::cout<<std::endl;
+			//std::cout<<std::endl;
 			count=0;
 		}
+	}
+}
+void refAdd(int refAr[],int size)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution dis(0,size-1);
+	for(int i=0;i<size;i++)
+	{
+		refAr[i]=dis(gen);
+	}
+        
+}
+void subRepeat(int ref[],int range)
+{
+	int count=0;
+	std::vector<int> left;
+	for( int p=0;p<range;p++)
+	{
+		for(int k=0;k<range;k++)
+		{
+			if(p!=ref[k])
+			{
+				count++;
+			}
+		}
+		if(count==range)
+		{
+			left.push_back(p);
+		}
+	}
+	for(int i=0;i<=left.size();i++)
+	{
+		std::cout<<ref[i]<<std::endl;
 	}
 }
 int main()
@@ -52,7 +54,8 @@ int main()
 	char baseCh[9]={'a','b','c','d','e','f','g','h','i'};
 	int baseAr[9]={0,1,2,3,4,5,6,7,8};
 	int refAr[9];
-	addRef(baseAr,refAr,9);
+	refAdd(refAr,9);
+	subRepeat(refAr,9);
 	printRef(refAr,9);
 	return 0;
 }
