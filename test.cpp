e5f66b6de71cace9a46611f4c0363f637da2bb79
Bowman7 <ui14628@gmail.com>
2022-08-09 09:44:57 +0530

    yeya

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<random>
-#include<vector>
 void printRef(int refAr[],int size)
 {
 	int count;
@@ -10,43 +9,36 @@ void printRef(int refAr[],int size)
 		count++;
 		if(count==3)
 		{
-			//std::cout<<std::endl;
+			std::cout<<std::endl;
 			count=0;
 		}
 	}
 }
 void refAdd(int refAr[],int size)
 {
+	int val=0;
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution dis(0,size-1);
 	for(int i=0;i<size;i++)
 	{
 		refAr[i]=dis(gen);
+		
 	}
-        
 }
-void subRepeat(int ref[],int range)
+void printAlp(int refAr[],char  baseCh[],int range)
 {
-	int count=0;
-	std::vector<int> left;
-	for( int p=0;p<range;p++)
+	int count=0;int val=0;
+	for(int k=0;k<range;k++)
 	{
-		for(int k=0;k<range;k++)
+		val=refAr[k];
+		std::cout<<" "<<baseCh[val]<<" ";
+		count++;
+		if(count==3)
 		{
-			if(p!=ref[k])
-			{
-				count++;
-			}
+			std::cout<<std::endl;
+			count=0;
 		}
-		if(count==range)
-		{
-			left.push_back(p);
-		}
-	}
-	for(int i=0;i<=left.size();i++)
-	{
-		std::cout<<ref[i]<<std::endl;
 	}
 }
 int main()
@@ -55,7 +47,7 @@ int main()
 	int baseAr[9]={0,1,2,3,4,5,6,7,8};
 	int refAr[9];
 	refAdd(refAr,9);
-	subRepeat(refAr,9);
-	printRef(refAr,9);
+	//printRef(refAr,9);
+	printAlp(refAr,baseCh,9);
 	return 0;
 }
