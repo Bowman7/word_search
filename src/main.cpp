80a4c717ef4f1b6649d0c0439e47f199f4c6a637
Bowman7 <ui14628@gmail.com>
2022-09-03 16:25:28 +0530

    yeya

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,36 @@
 class word
 {
 public:
-	word();
+	word(int[],int);
 	void run();
 private:
+	int sourceRandAr[9];
+	int sourceRandRange;
 	void processEvents();
 	void update();
 	void render();
+	void assignPos(int [],sf::Sprite [],int);
+	
 private:
 	sf::RenderWindow mWindow;
 	sf::Texture mTexture;
-	sf::Sprite mSprite;
+	sf::Sprite mSprite[9];
    
 };
-word::word()
-	:mWindow(sf::VideoMode(1000,1000),"cruel world ")
+word::word(int psourceRandAr[],int psourceRandRange)
+	:mWindow(sf::VideoMode(900,900),"cruel world "),mTexture(),mSprite()
 {
+	mTexture.loadFromFile("/home/batu/repos/word_search/src/word.png");
+	sourceRandRange=psourceRandRange;
+	for(int i=0;i<sourceRandRange;i++)
+	{
+		sourceRandAr[i]=psourceRandAr[i];
+	}
+	for(int k=0;k<9;k++)
+	{
+		mSprite[k].setTexture(mTexture);
+	}
+	assignPos(sourceRandAr,mSprite,9);
 	
 }
 void word::run()
@@ -56,14 +71,97 @@ void word::update()
 }
 void word::render()
 {
-	mWindow.clear();
+        mWindow.clear();
+        for(int i=0;i<9;i++)
+	{
+		mWindow.draw(mSprite[i]);
+	}
 	mWindow.display();
+	
+}
+void word::assignPos(int ar[],sf::Sprite sp[],int range)
+{
+	float posx,posy;
+	posx=0.f;posy=0.f;
+	int count =0;
+	while(count!=range)
+	{
+		if(posx==900.f)
+		{
+			posx=0;
+			posy+=300.f;
+		}
+		if(ar[count]==0)
+		{
+			sf::IntRect r2(0.f,0.f,300,300);
+			sp[0].setTextureRect(r2);
+			sp[0].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==1)
+		{
+			sf::IntRect r3(300.f,0.f,300,300);
+			sp[1].setTextureRect(r3);
+			sp[1].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==2)
+		{
+			sf::IntRect r4(600.f,0.f,300,300);
+			sp[2].setTextureRect(r4);
+			sp[2].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==3)
+		{
+			sf::IntRect r5(0.f,300.f,300,300);
+			sp[3].setTextureRect(r5);
+			sp[3].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==4)
+		{
+			sf::IntRect r6(300.f,300.f,300,300);
+			sp[4].setTextureRect(r6);
+			sp[4].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==5)
+		{
+			sf::IntRect r7(600.f,300.f,300,300);
+			sp[5].setTextureRect(r7);
+			sp[5].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==6)
+		{
+			sf::IntRect r8(0.f,600.f,300,300);
+			sp[6].setTextureRect(r8);
+			sp[6].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==7)
+		{
+			sf::IntRect r9(300.f,600.f,300,300);
+			sp[7].setTextureRect(r9);
+			sp[7].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		if(ar[count]==8)
+		{
+			sf::IntRect r10(600.f,600.f,300,300);
+			sp[8].setTextureRect(r10);
+			sp[8].setPosition(posx,posy);
+			posx+=300.f;
+		}
+		count++;
+	}
 }
 int main()
 {
 	int refAr[9];
 	run(refAr,9);
-	word world;
+	word world(refAr,9);
 	world.run();
 	return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,181 @@
+#include<iostream>
+#include<random>
+#include<vector>
+void printRef(int refAr[],int size)
+{
+	int count=0;
+	for(int i=0;i<size;i++)
+	{
+		std::cout<<" "<<refAr[i]<<" ";
+		count++;
+		if(count==3)
+		{
+			std::cout<<std::endl;
+			count=0;
+		}
+	}
+}
+void refAdd(int refAr[],int size)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution dis(0,size-1);
+        for(int i=0;i<size;i++)
+	{
+		refAr[i]=dis(gen);
+	}
+}
+void printAlp(int refAr[],char  baseCh[],int range)
+{
+	int count=0;int val=0;
+	for(int k=0;k<range;k++)
+	{
+		val=refAr[k];
+		std::cout<<" "<<baseCh[val]<<" ";
+		count++;
+		if(count==3)
+		{
+			std::cout<<std::endl;
+			count=0;
+		}
+	}
+}
+void missingNo(int refAr[],int range,int misAr[])
+{
+	int count=0;int p=0;
+	
+	for(int i=0;i<range;i++)
+	{
+		for(int j=0;j<range;j++)
+		{
+			if(i==refAr[j])
+			{
+				count++;
+			}
+			
+		}
+		if(count==0)
+			{
+				misAr[p]=i;
+				p++;
+			}
+		count=0;
+	}
+}
+void printMiss(std::vector<int> &misNo, int mSize)
+{
+	std::cout<<"Missing Nos: ";
+	for(int i=0;i<mSize;i++)
+	{
+		std::cout<<misNo[i];
+	}
+}
+void missingNoVec(int refAr[],int range, std::vector<int> &misNo ,int *mSize)
+{
+	int count=0;int val=0;
+        for(int i=0;i<range;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			if(i==refAr[j])
+			{
+				count++;
+			}
+		}
+		if(count==0)
+		{
+			//std::cout<<i<<std::endl;
+			misNo.push_back(i);
+			val++;
+		}
+		count=0;
+	}
+	*mSize=val;
+}
+void getRepeating(int refAr[],int size,std::vector<int> &repeatingPos,int *rSize)
+{
+	//std::cout<<"adsjf";
+	int count=0;int vecSize=0;
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			if(i==refAr[j])
+			{
+				count++;
+				if(count==2)
+				{
+					repeatingPos.push_back(j);
+					vecSize++;
+				}
+				if(count==3)
+				{
+					repeatingPos.push_back(j);
+					vecSize++;
+				}
+				if(count==4)
+				{
+					repeatingPos.push_back(j);
+					vecSize++;
+				}
+				if(count>4)
+				{
+					repeatingPos.push_back(j);
+					vecSize++;
+				}
+			}
+		        
+		}
+		count =0;
+	}
+	*rSize=vecSize;
+}
+void printRepeat(std::vector<int> &repeatingPos,int refAr[],int rSize)
+{
+	//std::cout<<"rsize:"<<rSize;
+	int pos=0;
+	std::cout<<std::endl;
+	std::cout<<"Repeating numbers: ";
+	for(int i=0;i<rSize;i++)
+	{	
+		pos=repeatingPos[i];
+		std::cout<<refAr[pos]<<" ";
+	}
+}
+void replaceRepeat(std::vector<int> &repeatingPos,int refAr[],int rSize,std::vector<int> &misNo)
+{
+	int val,pal;
+	for(int i=0;i<rSize;i++)
+	{
+		pal=repeatingPos[i];
+		val=misNo[i];
+		refAr[pal]=val;
+	}
+	std::cout<<std::endl;
+}
+void run(int refAr[],int size)
+{
+	int misAr[10];int mSize=0;int rSize;
+	std::vector<int> misNo;
+	std::vector<int> repeatingPos;
+	char baseCh[size]={'a','b','c','d','e','f','g','h','i'};
+	int baseAr[size]={0,1,2,3,4,5,6,7,8};
+	//int refAr[size];
+	refAdd(refAr,size);
+	printRef(refAr,size);
+	missingNoVec(refAr,size,misNo,&mSize);
+	printMiss(misNo,mSize);
+        getRepeating(refAr,size,repeatingPos,&rSize);
+	printRepeat(repeatingPos,refAr,rSize);
+	replaceRepeat(repeatingPos,refAr,rSize,misNo);
+	printRef(refAr,size);
+	printAlp(refAr,baseCh,size);
+	
+	//printAlp(refAr,baseCh,9);
+//	return 0;
+}
+int main()
+{
+	int refAr[9];
+	run(refAr,9);
+}
